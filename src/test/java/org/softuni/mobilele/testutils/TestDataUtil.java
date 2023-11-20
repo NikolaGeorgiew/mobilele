dd7ddcc0a395cf7666b28c5b25a98abaa2342a58
NikolaGeorgiew <nikolageorgiev2000n@gmail.com>
2023-11-20 23:16:33 +0200

    Preparation for deleting test.

--- src/test/java/org/softuni/mobilele/testutils/TestDataUtil.java
+++ src/test/java/org/softuni/mobilele/testutils/TestDataUtil.java
@@ -1,17 +1,31 @@
 package org.softuni.mobilele.testutils;
 
 import org.softuni.mobilele.model.entity.ExchangeRate;
+import org.softuni.mobilele.model.entity.Offer;
+import org.softuni.mobilele.model.entity.UserEntity;
+import org.softuni.mobilele.model.enums.UserRoleEnum;
 import org.softuni.mobilele.repository.ExchangeRateRepository;
+import org.softuni.mobilele.repository.OfferRepository;
+import org.softuni.mobilele.repository.UserRepository;
+import org.softuni.mobilele.repository.UserRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Component
-public class TestData {
+public class TestDataUtil {
 
     @Autowired
     private ExchangeRateRepository exchangeRateRepository;
+
+    @Autowired
+    private OfferRepository offerRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserRoleRepository userRoleRepository;
 
     public void createExchangeRate(String currency, BigDecimal rate) {
         exchangeRateRepository.save(new ExchangeRate().setCurrency(currency).setRate(rate));
@@ -19,4 +33,8 @@ public class TestData {
     public void cleanAllTestData() {
         exchangeRateRepository.deleteAll();
     }
+//TODO:
+//    public String  createTestOffer(UserEntity owner) {
+//
+//    }
 }
